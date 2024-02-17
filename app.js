8175269e85d004bbe339c06b8063c00cab018fc5
timatima0314 <tima314@gmail.com>
2024-02-17 14:01:21 +0900

    dev_3

--- app.js
+++ app.js
@@ -10,10 +10,25 @@ setButtonA.addEventListener("click", () => {
   window.myAPI.setA(text);
 });
 setButtonB.addEventListener("click", () => {
+  var para = document.getElementById("json");
   const text = inputB.value;
+  console.log(text);
+  // let html = para.outerHTML;
+  // console.log(html);
+  let obj = text;
+  let json = JSON.stringify(obj);
   window.myAPI.setB(text);
 });
 setButtonC.addEventListener("click", () => {
   const text = inputC.value;
+  // let a = `  <div id='json'>
+  // <p>aaa</p>
+  // <p>bbb</p>
+  // </div>
+  // `;
+  // const rep = text.replace()
+  // const val = JSON.stringify(text, (k, v) =>
+  //   typeof v === "string" ? v.replace(/\n/g, "\r\n") : v
+  // );
   window.myAPI.setC(text);
 });
--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const db = require("electron-db");
 let val = "";
 const savePath = path.join("./database", "");
 var CryptoJS = require("crypto-js");
-
+const { request } = require("http");
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1080,
@@ -19,6 +19,7 @@ const createWindow = () => {
     title: "マイアプリ",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
     },
   });
   mainWindow.webContents.openDevTools({ mode: "detach" });
@@ -51,8 +52,9 @@ const createWindow = () => {
     };
 
     let set = {
-      text: ciphertext,
+      text: text,
     };
+
     db.updateRow("Test", savePath, where, set, (succ, msg) => {
       // succ - boolean, tells if the call is successful
       console.log("Success: " + succ);
@@ -85,6 +87,7 @@ const createWindow = () => {
   //     }
   //   });
   // }
+  // const $ = (jQuery = require("jquery"));
 };
 app.whenReady().then(() => {
   globalShortcut.register("CommandOrControl+A", () => {
@@ -104,19 +107,39 @@ app.whenReady().then(() => {
         if (success) {
           var bytes = CryptoJS.AES.decrypt(data[1].text, "secret key 123");
           var originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-          await keyboard.type(originalText);
+          // console.log(data[1].text);
+          let a = data[1].text;
+          // console.log(val);
+          // const rep = text.replace();
+          // const str = "<link rel=\'stylesheet\' href=\'style.css\'>\n<title>マイアプリ</title>";
+          // const aaa = JSON.stringify(val);
+          // console.log("str",str);
+          // console.log('a',a)
+          const ii = a.replace(/\n/g, "\r\n");
+          // console.log(ii);
+          //   typeof v === "string" ? v.replace(/\n/g, "\r\n") : v
+          // );
+          // const json = '{"text": "<link rel=\'stylesheet\' href=\'style.css\'>\r<title>マイアプリ</title>"}';
+          // const obj = JSON.parse(json);
+          await keyboard.type(ii);
         } else {
           console.log("getAll failed");
         }
       });
     })();
   });
-  globalShortcut.register("CommandOrControl+C", () => {
+  globalShortcut.register("CommandOrControl+H", () => {
     (async () => {
       db.getAll("Test", savePath, async (success, data) => {
         if (success) {
-          await keyboard.type(data[2].text);
+          await keyboard.type(`<head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="Content-Security-Policy" content="default-src 'self'" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="style.css">
+          <title>マイアプリ</title>
+        </head>
+        `);
         } else {
           console.log("getAll failed");
         }
